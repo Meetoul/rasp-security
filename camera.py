fadb518daccef7f01b60d121bcdc5af98f85649b
Ihor Ivanov <toulmee@gmail.com>
2019-06-02 12:45:06 +0300

    Initial camera streaming setup
    Signed-off-by: Ihor Ivanov <toulmee@gmail.com>

--- camera.py
+++ camera.py
@@ -2,15 +2,26 @@ import subprocess
 import glob
 import os
 
-from config import CAPTURES_DIR, CAMERA_DEVICE
+from config import CAPTURES_DIR
 
 CAPTURE_SHELL_COMMAND = 'ffmpeg -f video4linux2 -i {} -ss 0:0:1 -frames 1 -strftime 1 "{}/%Y-%m-%d-%H-%M-%S_capture.jpg"'
 
+RECORD_SHELL_COMMAND = "ffmpeg -f video4linux2 -i {} -s 640x480 -r 15 -vcodec h264 -codec copy -an http://{}:{}/camera.ffm"
+
 class Camera:
 
+    def __init__(self, camera_device):
+        self.m_camera = camera_device
+
     def capture(self):
-        subprocess.run(CAPTURE_SHELL_COMMAND.format(CAMERA_DEVICE, CAPTURES_DIR), shell=True)
+        subprocess.run(CAPTURE_SHELL_COMMAND.format(m_camera, CAPTURES_DIR), shell=True)
         return self.last_capture()
+
+    def start_record(self):
+        subprocess.run(CAPTURE_SHELL_COMMAND.format(m_camera, CAPTURES_DIR), shell=True)
+
+    def start_streaming(self):
+        subprocess.run(CAPTURE_SHELL_COMMAND.format(m_camera, CAPTURES_DIR), shell=True)
 
     def last_capture(self):
         list_of_files = glob.glob('%s/*.jpg' % CAPTURES_DIR)
